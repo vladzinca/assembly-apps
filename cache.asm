935586df915fa37decd593842423d5c07371798c
vladzinca <vlad_zinca@yahoo.com>
2021-12-13 17:40:12 +0200

    v2.0 Final built before release

--- cache.asm
+++ cache.asm
@@ -7,25 +7,26 @@ TAG_BITS EQU 29 ; 32 - OFSSET_BITS
 
 section .text
     global load
-    extern printf
 
-;; void load(char* reg, char** tags, char cache[CACHE_LINES][CACHE_LINE_SIZE], char* address, int to_replace);
+;; void load(char* reg, char** tags, char cache[CACHE_LINES][CACHE_LINE_SIZE],
+;;           char* address, int to_replace);
 load:
     ;; DO NOT MODIFY
-    push ebp
-    mov ebp, esp
+    push    ebp
+    mov     ebp, esp
     pusha
 
-    mov eax, [ebp + 8]  ; address of reg
-    mov ebx, [ebp + 12] ; tags
-    mov ecx, [ebp + 16] ; cache
-    mov edx, [ebp + 20] ; address
-    mov edi, [ebp + 24] ; to_replace (index of the cache line that needs to be replaced in case of a cache MISS)
+    mov     eax, [ebp + 8]  ; address of reg
+    mov     ebx, [ebp + 12] ; tags
+    mov     ecx, [ebp + 16] ; cache
+    mov     edx, [ebp + 20] ; address
+    mov     edi, [ebp + 24] ; to_replace (index of the cache line that needs to be
+                        ; replaced in case of a cache MISS)
     ;; DO NOT MODIFY
 
     ;; TODO: Implment load
     ;; FREESTYLE STARTS HERE
-    
+
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
@@ -33,5 +34,3 @@ load:
     leave
     ret
     ;; DO NOT MODIFY
-
-
